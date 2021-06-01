34af615c007d0b672d2f0188af9851205bda5154
echerevko <43856956+echerevko@users.noreply.github.com>
2021-06-01 17:18:49 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import logo from "./logo.png"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        <p>Hello!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
