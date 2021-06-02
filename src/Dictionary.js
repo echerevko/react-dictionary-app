bb6fd59c53d7d1de607cbc81ab0f28e84693b322
echerevko <43856956+echerevko@users.noreply.github.com>
2021-06-02 13:47:52 +0200

    added the definition of the word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,16 +6,17 @@ import Results from "./Results"
 
 const Dictionary = () => {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
     
     function handleResponse(response) {
-        console.log(response.data[0]);
-        console.log(response.data[0].meanings[0].definitions[0].definition);
-
+        // console.log(response.data[0]);
+        // console.log(response.data[0].meanings[0].definitions[0].definition);
+        setResults(response.data[0]);
     }
 
     function search(event){
         event.preventDefault();
-        alert(`${keyword}`);
+        // alert(`${keyword}`);
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
     }
@@ -30,7 +31,7 @@ const Dictionary = () => {
         <div className="Dictionary">
          <form onSubmit={search}>
             <input type="search" autoFocus={true} onChange={handleKeywordChange}/>
-           <Results/> 
+           <Results results={results}/> 
          </form> 
         </div>
     )
--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const Results = () => {
-    return (
-        <div>
-            
+const Results = (props) => {
+    if (props.results) {
+        return (
+        <div className="Results">
+            <h2> {props.results.word}</h2>
+            {props.results.meanings.map(function(meaning,index) {
+                return meaning.definitions[0].definition;
+            })}
         </div>
-    )
+        )
+    } else {
+        return null;
+   }
+       
 }
 
 export default Results;
