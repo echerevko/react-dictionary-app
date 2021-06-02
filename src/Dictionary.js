194f79981ebe0626afada318843f776138601604
echerevko <43856956+echerevko@users.noreply.github.com>
2021-06-02 10:20:29 +0200

    Hooked integration api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,27 @@
 import React, {useState} from 'react'
-import "./Dictionary.css"
+import axios from "axios";
+import "./Dictionary.css";
+
 
 const Dictionary = () => {
     let [keyword, setKeyword] = useState("");
+    
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
+
     function search(event){
         event.preventDefault();
-        alert(`${keyword}`)
+        alert(`${keyword}`);
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeywordChange(event) {
         console.log(event.target.value);
         setKeyword(event.target.value);
     }
+
 
     return (
         <div className="Dictionary">
